49025c3f3547a26620fe3a59acad1094157b1c88
loclv <masamonedante@gmail.com>
2020-09-15 10:17:00 +0700

    replace xlsx with exceljs: fix

--- replace.js
+++ replace.js
@@ -12,34 +12,30 @@ const outputName = 'new';
 let workbook = new Excel.Workbook();
 
 function readWrite() {
-  workbook.xlsx
-    .readFile(`${outputName}.xlsx`)
-    .then(function () {
-      workbook.eachSheet(function (worksheet, sheetId) {
-        worksheet.eachRow(function (row, rowNumber) {
-          row.eachCell(function (cell, colNumber) {
-            let v = cell.value;
-            let regex = new RegExp(oldTxt, 'g');
-            // if the cell is a text cell with the old string, change it
-            if (v.includes(oldTxt)) cell.value = v.replace(regex, newTxt);
-          });
-          // Commit a completed row to stream
-          row.commit();
+  workbook.xlsx.readFile(`${inputName}.xlsx`).then(function () {
+    workbook.eachSheet(function (worksheet, sheetId) {
+      worksheet.eachRow(function (row, rowNumber) {
+        row.eachCell(function (cell, colNumber) {
+          let v = cell.value;
+          if (!v) return;
+          let regex = new RegExp(oldTxt, 'g');
+          // if the cell is a text cell with the old string, change it
+          if (v.includes(oldTxt)) cell.value = v.replace(regex, newTxt);
         });
+        // Commit a completed row to stream
+        row.commit();
       });
-
-      workbook.xlsx
-        .writeFile(`${outputName}.xlsx`)
-        .then(function () {
-          console.log('File is written');
-        })
-        .catch(function (err) {
-          console.log(`writeFile Err: ${err}.`);
-        });
-    })
-    .catch(function (err) {
-      console.log(`readFile Err: ${err}.`);
     });
+
+    workbook.xlsx
+      .writeFile(`${outputName}.xlsx`)
+      .then(function () {
+        console.log('File is written');
+      })
+      .catch(function (err) {
+        console.log(`writeFile Err: ${err}.`);
+      });
+  });
 }
 
 // new.txt will be created or overwritten by default.

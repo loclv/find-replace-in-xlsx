65cd32124b6e9e7abe330ef4a835214b943643af
loclv <masamonedante@gmail.com>
2020-09-15 09:48:57 +0700

    refactoring

--- replace.js
+++ replace.js
@@ -1,36 +1,38 @@
-var oldTxt = 'OLD';
-var newTxt = 'NEW';
-var inputName = 'original';
-var outputName = 'new';
+'use strict';
 
-var XLSX = require('xlsx');
+const XLSX = require('xlsx');
 
-var fs = require('fs');
+const fs = require('fs');
+
+const oldTxt = 'OLD';
+const newTxt = 'NEW';
+const inputName = 'original';
+const outputName = 'new';
 
 function findReplace() {
   /* read the file */
-  var workbook = XLSX.readFile(`${outputName}.xlsx`); // parse the file
-  var sheetNames = workbook.SheetNames;
+  const workbook = XLSX.readFile(`${outputName}.xlsx`); // parse the file
+  const sheetNames = workbook.SheetNames;
 
   sheetNames.forEach(function (y) {
-    var sheet = workbook.Sheets[y];
+    let sheet = workbook.Sheets[y];
 
     /* loop through every cell manually */
-    var range = XLSX.utils.decode_range(sheet['!ref']); // get the range
+    let range = XLSX.utils.decode_range(sheet['!ref']); // get the range
 
-    for (var R = range.s.r; R <= range.e.r; ++R) {
-      for (var C = range.s.c; C <= range.e.c; ++C) {
+    for (let R = range.s.r; R <= range.e.r; ++R) {
+      for (let C = range.s.c; C <= range.e.c; ++C) {
         /* find the cell object */
-        var cellRef = XLSX.utils.encode_cell({c: C, r: R}); // construct A1 reference for cell
+        let cellRef = XLSX.utils.encode_cell({c: C, r: R}); // construct A1 reference for cell
         if (!sheet[cellRef]) continue; // if cell doesn't exist, move on
 
-        var cell = sheet[cellRef];
+        let cell = sheet[cellRef];
 
         /* if the cell is a text cell with the old string, change it */
         if ((cell.t !== 's' && cell.t !== 'str') || !cell.v) continue; // skip if cell is not text
 
-        var v = cell.v;
-        var regex = new RegExp(oldTxt, 'g');
+        let v = cell.v;
+        let regex = new RegExp(oldTxt, 'g');
 
         if (v.includes(oldTxt)) cell.v = v.replace(regex, newTxt);
       }

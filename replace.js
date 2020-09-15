38d9ef8d8a0d1f3c164d63c5510742a7f5e77ad4
loclv <masamonedante@gmail.com>
2020-09-15 09:56:09 +0700

    refactoring

--- replace.js
+++ replace.js
@@ -12,8 +12,6 @@ const outputName = 'new';
 let workbook = new Excel.Workbook();
 
 function readWrite() {
-  /* read the file */
-
   workbook.xlsx
     .readFile(`${outputName}.xlsx`)
     .then(function () {
@@ -22,31 +20,34 @@ function readWrite() {
       sheetNames.forEach(function (y) {
         let sheet = workbook.Sheets[y];
 
-        /* loop through every cell manually */
-        let range = XLSX.utils.decode_range(sheet['!ref']); // get the range
+        let range = XLSX.utils.decode_range(sheet['!ref']);
 
         for (let R = range.s.r; R <= range.e.r; ++R) {
           for (let C = range.s.c; C <= range.e.c; ++C) {
-            /* find the cell object */
-            let cellRef = XLSX.utils.encode_cell({c: C, r: R}); // construct A1 reference for cell
-            if (!sheet[cellRef]) continue; // if cell doesn't exist, move on
+            let cellRef = XLSX.utils.encode_cell({c: C, r: R});
+            // if cell doesn't exist, move on
+            if (!sheet[cellRef]) continue;
 
             let cell = sheet[cellRef];
-
-            /* if the cell is a text cell with the old string, change it */
-            if ((cell.t !== 's' && cell.t !== 'str') || !cell.v) continue; // skip if cell is not text
+            // skip if cell is not text
+            if ((cell.t !== 's' && cell.t !== 'str') || !cell.v) continue;
 
             let v = cell.v;
             let regex = new RegExp(oldTxt, 'g');
-
+            // if the cell is a text cell with the old string, change it
             if (v.includes(oldTxt)) cell.v = v.replace(regex, newTxt);
           }
         }
       });
 
-      workbook.xlsx.writeFile(`${outputName}.xlsx`).catch(function (err) {
-        console.log(`writeFile Err: ${err}.`);
-      });
+      workbook.xlsx
+        .writeFile(`${outputName}.xlsx`)
+        .then(function () {
+          console.log('File is written');
+        })
+        .catch(function (err) {
+          console.log(`writeFile Err: ${err}.`);
+        });
     })
     .catch(function (err) {
       console.log(`readFile Err: ${err}.`);
@@ -54,9 +55,4 @@ function readWrite() {
 }
 
 // new.txt will be created or overwritten by default.
-fs.copyFile(`${inputName}.xlsx`, `${outputName}.xlsx`, (err) => {
-  if (err) throw err;
-  console.log(`${inputName}.xlsx was copied to ${outputName}.xlsx`);
-  console.log('read file');
-  readWrite();
-});
+readWrite();

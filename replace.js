85bd467d36c00f15f075673fbce2f812b553b87f
loclv <masamonedante@gmail.com>
2020-09-15 09:31:01 +0700

    replace xlsx with exceljs: refactoring

--- replace.js
+++ replace.js
@@ -1,48 +1,62 @@
-var oldTxt = 'OLD';
-var newTxt = 'NEW';
-var inputName = 'original';
-var outputName = 'new';
+'use strict';
 
-var XLSX = require('xlsx');
+const Excel = require('exceljs');
 
-var fs = require('fs');
+const fs = require('fs');
 
-function findReplace() {
+const oldTxt = 'OLD';
+const newTxt = 'NEW';
+const inputName = 'original';
+const outputName = 'new';
+
+let workbook = new Excel.Workbook();
+
+function readWrite() {
   /* read the file */
-  var workbook = XLSX.readFile(`${outputName}.xlsx`); // parse the file
-  var sheetNames = workbook.SheetNames;
 
-  sheetNames.forEach(function (y) {
-    var sheet = workbook.Sheets[y];
+  workbook.xlsx
+    .readFile(`${outputName}.xlsx`)
+    .then(function () {
+      let sheetNames = workbook.SheetNames;
 
-    /* loop through every cell manually */
-    var range = XLSX.utils.decode_range(sheet['!ref']); // get the range
+      sheetNames.forEach(function (y) {
+        let sheet = workbook.Sheets[y];
 
-    for (var R = range.s.r; R <= range.e.r; ++R) {
-      for (var C = range.s.c; C <= range.e.c; ++C) {
-        /* find the cell object */
-        var cellRef = XLSX.utils.encode_cell({c: C, r: R}); // construct A1 reference for cell
-        if (!sheet[cellRef]) continue; // if cell doesn't exist, move on
+        /* loop through every cell manually */
+        let range = XLSX.utils.decode_range(sheet['!ref']); // get the range
 
-        var cell = sheet[cellRef];
+        for (let R = range.s.r; R <= range.e.r; ++R) {
+          for (let C = range.s.c; C <= range.e.c; ++C) {
+            /* find the cell object */
+            let cellRef = XLSX.utils.encode_cell({c: C, r: R}); // construct A1 reference for cell
+            if (!sheet[cellRef]) continue; // if cell doesn't exist, move on
 
-        /* if the cell is a text cell with the old string, change it */
-        if ((cell.t !== 's' && cell.t !== 'str') || !cell.v) continue; // skip if cell is not text
+            let cell = sheet[cellRef];
 
-        var v = cell.v;
-        var regex = new RegExp(oldTxt, 'g');
+            /* if the cell is a text cell with the old string, change it */
+            if ((cell.t !== 's' && cell.t !== 'str') || !cell.v) continue; // skip if cell is not text
 
-        if (v.includes(oldTxt)) cell.v = v.replace(regex, newTxt);
-      }
-    }
-  });
+            let v = cell.v;
+            let regex = new RegExp(oldTxt, 'g');
 
-  XLSX.writeFile(workbook, `${outputName}.xlsx`);
+            if (v.includes(oldTxt)) cell.v = v.replace(regex, newTxt);
+          }
+        }
+      });
+
+      workbook.xlsx.writeFile(`${outputName}.xlsx`).catch(function (err) {
+        console.log(`writeFile Err: ${err}.`);
+      });
+    })
+    .catch(function (err) {
+      console.log(`readFile Err: ${err}.`);
+    });
 }
 
 // new.txt will be created or overwritten by default.
 fs.copyFile(`${inputName}.xlsx`, `${outputName}.xlsx`, (err) => {
   if (err) throw err;
   console.log(`${inputName}.xlsx was copied to ${outputName}.xlsx`);
-  findReplace();
+  console.log('read file');
+  readWrite();
 });
